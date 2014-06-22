1bb6abb9204955aede52a8631aed49a607b96f21
ShadowMech <basilis.lab@gmail.com>
2014-06-22 23:18:49 +0300

    First, not working version

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-   #test commit
-
+   cache<-NULL
+   set <- function(y){
+      x <<- y
+      cache<<- NULL
+   }
+   get <- function() x
+   setCached <- function(value) cache <<-value
+   getCached <- function() cache
+   list(set = set,get = get,
+        setCached = setCached,
+        getCached = getCached)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(chacheContainer, ...) {
         ## Return a matrix that is the inverse of 'x'
+   inv<-chacheContainer$getCached()
+   if(!is.null(inv)){
+      message("getting cached data")
+      return(inv)
+   }
+   data <- chacheContainer$get()
+   inv<-solve(data,...)
+   chacheContainer$setCached(inv)
+   inv   
 }
+M=rbind(c(1, -1/4), c(-1/4, 1)) 
+cm<-makeCacheMatrix(M)
+print(cm$get())
+cacheSolve(x=cm)
